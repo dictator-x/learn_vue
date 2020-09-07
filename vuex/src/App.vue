f956d9cea17d9756a7c199bb12a5f0777fa42442
dictator-x <dictator.xwu@gmail.com>
2020-09-07 17:29:54 -0400

    vuex project initial

---
+++ vuex/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'message'
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
