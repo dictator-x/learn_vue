6e1ec769ef3f60dd738fd2580f566c43c3be40bd
dictator-x <dictator.xwu@gmail.com>
2020-09-07 12:43:44 -0400

    add router for tabbar

--- tabbar/src/App.vue
+++ tabbar/src/App.vue
@@ -1,27 +1,28 @@
 <template>
   <div id="app" >
     <tab-bar>
-      <tab-bar-item>
+      <tab-bar-item path="/home">
         <img slot="item-icon" src="@/assets/img/tabbar/home.png" alt="">
         <img slot="item-icon-active" src="@/assets/img/tabbar/home_select.png" alt="">
         <div slot="item-text">Main</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="/category">
         <img slot="item-icon" src="@/assets/img/tabbar/categories.png" alt="">
         <img slot="item-icon-active" src="@/assets/img/tabbar/catogeries_select.png" alt="">
         <div slot="item-text">Category</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="/cart">
         <img slot="item-icon" src="@/assets/img/tabbar/shoppingCart.png" alt="">
         <img slot="item-icon-active" src="@/assets/img/tabbar/shoppingCart_select.png" alt="">
         <div slot="item-text">Cart</div>
       </tab-bar-item>
-      <tab-bar-item>
+      <tab-bar-item path="/profile">
         <img slot="item-icon" src="@/assets/img/tabbar/profile.png" alt="">
         <img slot="item-icon-active" src="@/assets/img/tabbar/profile_select.png" alt="">
         <div slot="item-text">Profile</div>
       </tab-bar-item>
     </tab-bar>
+    <router-view></router-view>
   </div>
 </template>
 
--- tabbar/src/router/index.js
+++ tabbar/src/router/index.js
@@ -1,7 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const Home = () => import('@/views/home/Home.vue')
+const Category = () => import('@/views/category/Category.vue')
+const Cart = () => import('@/views/cart/Cart.vue')
+const Profile = () => import('@/views/profile/Profile.vue')
+
 Vue.use(Router)
 
-export default new Router({
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  }
+]
+
+const router = new Router({
+  routes
 })
+
+export default router
