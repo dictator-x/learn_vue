367043e8af3261093ba7e65eab49e6c50704e130
dictator-x <dictator.xwu@gmail.com>
2020-09-07 13:37:56 -0400

    component to MainTabBar

--- tabbar/src/App.vue
+++ tabbar/src/App.vue
@@ -1,39 +1,16 @@
 <template>
   <div id="app" >
-    <tab-bar>
-      <tab-bar-item path="/home" activeColor="blue">
-        <img slot="item-icon" src="@/assets/img/tabbar/home.png" alt="">
-        <img slot="item-icon-active" src="@/assets/img/tabbar/home_select.png" alt="">
-        <div slot="item-text">Main</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category" activeColor="blue">
-        <img slot="item-icon" src="@/assets/img/tabbar/categories.png" alt="">
-        <img slot="item-icon-active" src="@/assets/img/tabbar/catogeries_select.png" alt="">
-        <div slot="item-text">Category</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart" activeColor="blue">
-        <img slot="item-icon" src="@/assets/img/tabbar/shoppingCart.png" alt="">
-        <img slot="item-icon-active" src="@/assets/img/tabbar/shoppingCart_select.png" alt="">
-        <div slot="item-text">Cart</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile" activeColor="blue">
-        <img slot="item-icon" src="@/assets/img/tabbar/profile.png" alt="">
-        <img slot="item-icon-active" src="@/assets/img/tabbar/profile_select.png" alt="">
-        <div slot="item-text">Profile</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import TabBar from '@/components/tabbar/TabBar.vue'
-  import TabBarItem from './components/tabbar/TabBarItem.vue'
+  import MainTabBar from '@/components/MainTabBar.vue'
   export default {
     name: 'App',
     components: {
-      TabBar,
-      TabBarItem
+      MainTabBar
     }
   }
 </script>
---
+++ tabbar/src/components/MainTabBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="blue">
+      <img slot="item-icon" src="@/assets/img/tabbar/home.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/home_select.png" alt="">
+      <div slot="item-text">Main</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="blue">
+      <img slot="item-icon" src="@/assets/img/tabbar/categories.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/catogeries_select.png" alt="">
+      <div slot="item-text">Category</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="blue">
+      <img slot="item-icon" src="@/assets/img/tabbar/shoppingCart.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/shoppingCart_select.png" alt="">
+      <div slot="item-text">Cart</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="blue">
+      <img slot="item-icon" src="@/assets/img/tabbar/profile.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/tabbar/profile_select.png" alt="">
+      <div slot="item-text">Profile</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from '@/components/tabbar/TabBar.vue'
+  import TabBarItem from '@/components/tabbar/TabBarItem.vue'
+  export default {
+    name: 'MainTabBar',
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
