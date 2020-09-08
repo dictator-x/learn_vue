f1fe3f27ac15d7a052ef9437d186c13f89516b98
dictator-x <dictator.xwu@gmail.com>
2020-09-07 20:00:19 -0400

    vuex project initial

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <div id="app">
-    <h2>{{ message }}</h2>
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="add">+</button>
+    <button @click="sub">-</button>
+    <hello-vuex></hello-vuex>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  data() {
-    return {
-      message: 'message'
+  import HelloVuex from '@/components/HelloVuex.vue'
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: 'message',
+      }
+    },
+    methods: {
+      add() {
+        this.$store.commit('increment')
+      },
+      sub() {
+        this.$store.commit('decrement')
+      }
+    },
+    components: {
+      HelloVuex
     }
-  },
-  components: {
   }
-}
 </script>
 
 <style>
